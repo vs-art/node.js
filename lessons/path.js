8190436f9fbb4e090675555c60b7b4ff89eff588
Vasyukov Sergey <s.vasyukoff@gmail.com>
2023-10-21 00:42:02 +0300

    path

---
+++ lessons/path.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+// use "join" for build paths
+console.log('join ex1:\r\n', path.join('dir1', 'dir2', 'dir3', 'file.js'));
+console.log('join ex2:\r\n', path.join('dir1', 'dir2', '..', 'dir3', 'file.js'));
+console.log('join ex3:\r\n', path.join('dir1', 'dir2', '.', 'dir3', 'file.js'));
+console.log('join ex4:\r\n', path.join(__dirname, 'dir1', 'dir2', 'dir3', 'file.js'));
+
+// use "resolve" for build paths
+console.log('resolve ex1:\r\n', path.resolve('dir1', 'dir2', 'dir3', 'file.js'));
+console.log('resolve ex2:\r\n', path.resolve('dir1', 'dir2', '..', 'dir3', 'file.js'));
+console.log('resolve ex3:\r\n', path.resolve('dir1', 'dir2', '.', 'dir3', 'file.js'));
+console.log('resolve ex4:\r\n', path.resolve(__dirname, 'dir1', 'dir2', 'dir3', 'file.js'));
+
+// parse path
+console.log('parse ex1:\r\n', path.parse(path.join('dir1', 'dir2', 'dir3', 'file.js')));
+console.log('parse ex2:\r\n', path.parse(path.resolve('dir1', 'dir2', 'dir3', 'file.js')));
+
+// additional features
+console.log('sep', path.sep);
+console.log('basename', path.basename(path.resolve('dir1', 'dir2', 'dir3', 'file.js')));
+console.log('extname', path.extname(path.resolve('dir1', 'dir2', 'dir3', 'file.js')));
